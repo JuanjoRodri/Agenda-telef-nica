b229730738ab9efa5e01d16a458fa583ca7d4c92
JuanjoRodri <rempujas@gmail.com>
2024-09-17 12:31:28 +0200

    Primeros pasos
    Primeros pasos

---
+++ src/main/java/com/mycompany/agendatelefonica/AgendaTelefonica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agendatelefonica;
+
+/**
+ *
+ * @author juanj
+ */
+public class AgendaTelefonica {
+
+    public static void main(String[] args) {
+        System.out.println("Aplicación de Agenda:Construye una aplicación de agenda que permita a los usuarios añadir contactos, organizarlos y buscar información. Practicarás la manipulación de objetos y estructuras de datos más complejas.");
+    }
+}
