86e8863e8643ad640cb3232819edddf0f840af42
JuanjoRodri <rempujas@gmail.com>
2024-09-19 20:14:12 +0200

    Primeros toques de codigo
    Primeros toques por falta de tiempo

---
+++ src/main/java/CRUD/Contacto.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CRUD;
+
+/**
+ *
+ * @author juanj
+ */
+public class Contacto {
+    int id_contacto;
+    String nombre;
+    String apellido1;
+    String apellido2;
+    int telefono;
+    String correoElectronico;
+    String empresa;
+    String informacionAdicional;
+
+    public int getId_contacto() {
+        return id_contacto;
+    }
+
+    public void setId_contacto(int id_contacto) {
+        this.id_contacto = id_contacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getInformacionAdicional() {
+        return informacionAdicional;
+    }
+
+    public void setInformacionAdicional(String informacionAdicional) {
+        this.informacionAdicional = informacionAdicional;
+    }
+}
--- src/main/java/CRUD/Read.java
+++ src/main/java/CRUD/Read.java
@@ -8,6 +8,65 @@ package CRUD;
  *
  * @author juanj
  */
-public class Read {
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Read  {
     
+
+    public Contacto leerContacto (int id_contacto){
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/tareas";
+        String user = "root";
+        String pass = "";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        Contacto c = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, pass);
+
+            String query = "SELECT * FROM contacto WHERE id_contacto = ?";
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id_contacto);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                c = new Contacto();
+                c.setId_contacto(rs.getInt("id_contacto"));
+                c.setNombre(rs.getString("nombre"));
+                c.setApellido1(rs.getString("apellido1"));
+                c.setApellido2(rs.getString("apellido2"));
+                c.setTelefono(rs.getInt("telefono"));
+                c.setCorreoElectronico(rs.getString("correo_electronico"));
+                c.setEmpresa(rs.getString("empresa"));
+                c.setInformacionAdicional(rs.getString("informacion"));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Contacto.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (ps != null) ps.close();
+                if (conn != null) conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Tarea.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return t;
+    }  
 }
